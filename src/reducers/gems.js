78257b262bfcff33dd7ac1646c4b8bf1efcec2df
Varun Vachhar <varunvachhar@gmail.com>
2016-08-15 21:50:20 -0400

    single gem

--- src/reducers/gems.js
+++ src/reducers/gems.js
@@ -7,19 +7,18 @@ import {
 import { fromJS } from 'immutable';
 import polygon from '../utils/shapes';
 
-const sides = [3, 4, 5, 6, 10];
+const gemSize = 10;
+const gemSides = 6;
 
 const INITIAL_STATE = fromJS({
   glowMixer: PATRIOT_CLRS.bg,
-  gems: sides.map((side, idx) => {
-    return {
-      idx: idx,
-      vertices: polygon(side, 5),
-      location: comfortable(idx),
-      color: PATRIOT_CLRS.gems[idx],
-      isGlowing: false,
-    };
-  }),
+  gems: [{
+    idx: 0,
+    vertices: polygon(gemSides, gemSize),
+    location: { x: 50, y: 50 },
+    color: PATRIOT_CLRS.gems[0],
+    isGlowing: false,
+  }],
 });
 
 function gemReducer(state = INITIAL_STATE, action = {}) {
@@ -50,29 +49,10 @@ export default gemReducer;
 
 
 /**
- * Helpers for resizing the gems
+ * Helper for resizing the gems
  */
-function cozy(idx) {
-  return { x: 7.5 + idx * 20.5, y: 50 };
-}
-
-function comfortable(idx) {
-  return { x: 20 + idx * 15, y: 50 };
-}
-
-function resizeGem(idx, size) {
-  return {
-    vertices: polygon(sides[idx], size),
-    location: size === 4 ? cozy(idx) : comfortable(idx),
-  };
-}
-
 function resizeGems(width, state) {
-  const size = (width > 600) ? 5 : 4;
-
   return state.update('gems', gems => {
-    return gems.map((g, i) => {
-      return g.merge( fromJS( resizeGem(i, size) ) );
-    });
+    return gems.map(gem => gem.merge({ vertices: polygon(gemSides, gemSize) }));
   });
 }
